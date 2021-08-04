f95901aa762ec8d8bcc5a6583b22cb45517bc3b1
unknown <mustafizmunna173@gmail.com>
2021-08-03 18:24:15 -0700

    deleted

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.use(express.json({limit:'30mb' , extended: true}))
 app.use(express.urlencoded({limit: '30mb' , extended: true}))
 app.use(cors())
 app.use('/posts',router)
-app.get('/' , (req,res) => {
-    res.send('hello from user post')
-})
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("client/build"))
